8ff640a19b8fa89a2f7277e1f31127df08beb33f
dnjarin <asaguero02@gmail.com>
2025-11-10 00:30:07 -0400

    Update

--- src/routes/pagina-fija.svelte
+++ src/routes/pagina-fija.svelte
@@ -6,4 +6,5 @@
 
 <Card title="Contenido Estático" subtitle="Página requerida">
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea a ex quisquam doloremque adipisci nisi nesciunt facilis, nemo, architecto rerum ab laboriosam voluptatem tempore provident quae consequatur magni accusamus assumenda.</p>
+  <span>Made By. Asdrubal Aguero</span>
 </Card>
